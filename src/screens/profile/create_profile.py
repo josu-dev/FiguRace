05122e795850b3eae33913e06024e0989f0c8947
LUCAS ANDRES,GALLARDO FLORIDO <gallardolucas003@gmail.com>
2022-05-11 16:11:53 -0300

    refactored names

--- src/screens/profile/create_profile.py
+++ src/screens/profile/create_profile.py
@@ -3,58 +3,53 @@ from src.handlers.layout import Screen
 from src.handlers import observer
 from src.screens.profile import profile
 from src.handlers import layout
+from src.handlers.theme import theme
 SCREEN_NAME = '-CREATE-PROFILE-'
-BACK_GROUND_COLOR = '#112B3C'
-BUTTON_COLOR = '#6FC5FF'
-BUTTON_HOVER_COLOR = '#5A9ECC'
-TEXT_BUTTON_COLOR = '#243F50'
-TITLE_COLOR = '#2D8BC5'
-TEXT_FONT = ('System', 45)
 
 _screen_main_title = sg.Text(SCREEN_NAME, size=500,
-                             background_color=BACK_GROUND_COLOR,
+                             background_color=theme.BG_BASE,
                              font=('Segoe Script', 45),
-                             text_color=TITLE_COLOR,
+                             text_color=theme.TEXT_ACCENT,
                              pad=0)
 
 _create_profile_layout = [
     [
         sg.Text('Nick', size=(4, 1),
-                background_color=BACK_GROUND_COLOR,
-                font=TEXT_FONT, pad=(5, 35)),
+                background_color=theme.BG_BASE,
+                font=(theme.FONT_FAMILY,45), pad=(5, 35)),
         sg.Input(size=(20, 15),
                  do_not_clear=False,
-                 background_color=BACK_GROUND_COLOR,
-                 font=('System', 30),
+                 background_color=theme.BG_BASE,
+                 font=(theme.FONT_FAMILY, 30),
                  text_color='white')
     ],
     [
         sg.Text('Age', size=(4, 1),
-                background_color=BACK_GROUND_COLOR,
-                font=TEXT_FONT, pad=(5, 35)),
+                background_color=theme.BG_BASE,
+                font=(theme.FONT_FAMILY,45), pad=(5, 35)),
         sg.Input(size=(20, 10),
                  do_not_clear=False,
-                 background_color=BACK_GROUND_COLOR,
+                 background_color=theme.BG_BASE,
                  font=('System', 30),
                  text_color='white')
     ],
     [
         sg.Text('Gender', size=(7, 1),
-                background_color=BACK_GROUND_COLOR,
-                font=TEXT_FONT, pad=(5, 20)),
+                background_color=theme.BG_BASE,
+                font=(theme.FONT_FAMILY,45), pad=(5, 20)),
         sg.Combo(('Female', 'Male', 'Undefined', 'Other'),
                  'Female',
                  background_color='#8DC3E4',
                  font=('System', 30),
                  readonly=True,
-                 text_color=BACK_GROUND_COLOR)
+                 text_color=theme.BG_BASE)
     ],
     [
         sg.Button('Save', key='-SAVE-',
                   border_width=15, size=(15, 1),
-                  button_color=(TEXT_BUTTON_COLOR, BUTTON_COLOR),
-                  mouseover_colors=BUTTON_HOVER_COLOR,
-                  font=('System', 20),
+                  button_color=(theme.TEXT_BUTTON, theme.BG_BUTTON),
+                  mouseover_colors=theme.BG_BUTTON_HOVER,
+                  font=(theme.FONT_FAMILY, 20),
                   pad=(10, 5))
     ]
 ]
@@ -63,9 +58,9 @@ _turn = sg.Button('<--',
                   key= f'{layout.GOTO_VIEW } -PROFILE-',
                   border_width=15,
                   size=(7, 0),
-                  button_color=(TEXT_BUTTON_COLOR, BUTTON_COLOR),
-                  mouseover_colors=BUTTON_HOVER_COLOR,
-                  font=('System', 20),
+                  button_color=(theme.TEXT_BUTTON, theme.BG_BUTTON),
+                  mouseover_colors=theme.BG_BUTTON_HOVER,
+                  font=(theme.FONT_FAMILY, 20),
                   pad=20)
 
 _screen_layout = [
@@ -74,7 +69,7 @@ _screen_layout = [
     ],
     [
         sg.Column(_create_profile_layout,
-                  background_color=BACK_GROUND_COLOR,
+                  background_color=theme.BG_BASE,
                   expand_x=True,
                   element_justification='c')
     ],
@@ -83,13 +78,11 @@ _screen_layout = [
     ]
 ]
 
-
 def function_to_execute_on_event() -> None:
     # This function calls updates on database, updates elements of ui, or do other stuff
     pass
 
 # observer.subscribe('-EVENT-TYPE-EVENT-EMITTER-', function_to_execute_on_event)
-
 
 def reset(*args):
     # Funcions
@@ -101,11 +94,10 @@ screen = Screen(
     reset
 )
 
-
 def main() -> None:
     window = sg.Window('Figurace - ' + SCREEN_NAME,
                        _screen_layout,
-                       background_color=BACK_GROUND_COLOR,
+                       background_color=theme.BG_BASE,
                        resizable=True).finalize()
     window.Maximize()
     while True:
@@ -116,7 +108,6 @@ def main() -> None:
         if events == sg.WIN_CLOSED:
             break
     window.close()
-
 
 if __name__ == '__main__':
     main()
--- src/screens/profile/profile.py
+++ src/screens/profile/profile.py
@@ -3,31 +3,26 @@ from src.handlers.layout import Screen
 from src.handlers import observer, layout
 from src.screens.profile import select_profile, create_profile
 from src.screens import menu
-
+from src.handlers.theme import theme
 SCREEN_NAME = '-PROFILE-'
-BACKGROUND_COLOR = '#112B3C'
-BUTTON_COLOR = '#6FC5FF'
-TEXT_BUTTON_COLOR = '#243F50'
-TITLE_COLOR = '#2D8BC5'
-TEXT_FONT = ('System', 30)
-BUTTON_HOVER_COLOR = '#5A9ECC'
 _screen_main_title = sg.Text(SCREEN_NAME, size=500,
-                             background_color=BACKGROUND_COLOR,
+                             background_color=theme.BG_BASE,
                              font=('Segoe Script', 45),
-                             text_color=TITLE_COLOR,
+                             text_color=theme.TEXT_ACCENT,
                              pad=0)
+
 _profile_layout = [
     [
-        sg.VPush(background_color=BACKGROUND_COLOR)
+        sg.VPush(background_color=theme.BG_BASE)
     ],
     [
         sg.Button('Create Profile',
                   key=f'{layout.GOTO_VIEW} {create_profile.SCREEN_NAME}',
                   border_width=20,
                   size=(15, 1),
-                  button_color=(TEXT_BUTTON_COLOR, BUTTON_COLOR),
-                  mouseover_colors=BUTTON_HOVER_COLOR,
-                  font=TEXT_FONT, pad=7)
+                  button_color=(theme.TEXT_BUTTON,theme.BG_BUTTON),
+                  mouseover_colors=theme.BG_BUTTON_HOVER,
+                  font=(theme.FONT_FAMILY,30), pad=7)
     ],
 
     [
@@ -35,21 +30,21 @@ _profile_layout = [
                   key=f'{layout.GOTO_VIEW} {select_profile.SCREEN_NAME}',
                   border_width=20,
                   size=(15, 1),
-                  button_color=(TEXT_BUTTON_COLOR, BUTTON_COLOR),
-                  mouseover_colors=BUTTON_HOVER_COLOR,
-                  font=TEXT_FONT, pad=7)
+                  button_color=(theme.TEXT_BUTTON,theme.BG_BUTTON),
+                  mouseover_colors=theme.BG_BUTTON_HOVER,
+                  font=(theme.FONT_FAMILY,30), pad=7)
     ],
     [
-        sg.VPush(background_color=BACKGROUND_COLOR)
+        sg.VPush(background_color=theme.BG_BASE)
     ]
 ]
 
 _turn = sg.Button('<--', key=f'{layout.GOTO_VIEW} {menu.SCREEN_NAME}',
                   border_width=15,
                   size=(7, 0),
-                  button_color=(TEXT_BUTTON_COLOR, BUTTON_COLOR),
-                  mouseover_colors=BUTTON_HOVER_COLOR,
-                  font=('System', 20), pad=20)
+                  button_color=(theme.TEXT_BUTTON,theme.BG_BUTTON),
+                  mouseover_colors=theme.BG_BUTTON_HOVER,
+                  font=(theme.FONT_FAMILY, 20), pad=20)
 
 _screen_layout = [
     [
@@ -57,7 +52,7 @@ _screen_layout = [
     ],
     [
         sg.Column(_profile_layout,
-                  background_color=BACKGROUND_COLOR,
+                  background_color=theme.BG_BASE,
                   expand_y=True, expand_x=True,
                   element_justification='c')
     ],
@@ -70,13 +65,11 @@ _screen_layout = [
 
 # _relative_size =
 
-
 def function_to_execute_on_event() -> None:
     # This function calls updates on database, updates elements of ui, or do other stuff
     pass
 
 # observer.subscribe('-EVENT-TYPE-EVENT-EMITTER-', function_to_execute_on_event)
-
 
 def reset(*args):
     # Funcions
@@ -88,10 +81,9 @@ screen = Screen(
     reset
 )
 
-
 def main() -> None:
     window = sg.Window('Figurace - ' + SCREEN_NAME,
-                       _screen_layout, background_color=BACKGROUND_COLOR,
+                       _screen_layout, background_color=theme.BG_BASE,
                        resizable=True).finalize()
     window.Maximize()
     while True:
@@ -105,7 +97,6 @@ def main() -> None:
         if events == sg.WIN_CLOSED:
             break
     window.close()
-
 
 if __name__ == '__main__':
     main()
