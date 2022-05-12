794f6d3f17a2064be85cd467f93147522181747f
Josue,Suarez <joosu16@gmail.com>
2022-05-12 10:43:44 -0300

    Test on jupyter notebook

--- section_dataset/base_datasets/Programa.py
+++ section_dataset/base_datasets/Programa.py
@@ -1,7 +1,8 @@
 import os
 import csv
 
-words_to_upper = ["Edm", "Dfw", "Uk", "R&b", "Lgbtq+"]
+UPPER_GENDERS = ["EDM", "DFW", "UK", "R&B", "LGBTQ+"]
+TITLE_GENDERS = ["K-Pop"]
 
 path = os.path.dirname(os.path.realpath("."))
 path_file = os.path.join(path,"grupo27", "section_dataset", "base_datasets", 'Spotify_2010-2019_Top_100.csv')
@@ -9,49 +10,17 @@ path_newfile = os.path.join(path,"grupo27", "section_dataset", "base_datasets", 
 
 with open(path_file,"r", encoding="utf-8") as File:  
     csv_reader = csv.reader(File, delimiter=',')
-    #encabezado = next(csv_reader)
     datos = list(csv_reader)
 
-with open(path_newfile, "w",encoding="utf-8") as NewFile:
+with open(path_newfile, "w",encoding="utf-8",newline='') as NewFile:
     csv_writer = csv.writer(NewFile)
-    #csv_writer.writerows(datos)
-    #print(datos)
     for row in datos:
-        row[2] = row[2].split()
-        # A todos los datos pongo la primera letra en mayuscula
-        for index, value in enumerate(row[2]):
-            row[2][index] = row[2][index].replace(value[0],value[0].upper()) 
-        # Paso la lista a una cadena
-        
-        #print(row[2][0])
-        try: 
-            word_to_change = words_to_upper.index(row[2][0])
-            row[2][0] = words_to_upper[word_to_change].upper()
+        genders = row[2].split()
+        for index, gender in enumerate(genders):
+            if gender.upper() in UPPER_GENDERS:
+                genders[index]= gender.upper()
+            else:
+                genders[index]= gender.title()
 
-        except (ValueError , IndexError):
-            pass
-
-        row[2] = " ".join(row[2])
-        #print(row[2])
+        row[2] = " ".join(genders)
         csv_writer.writerow(row)
-
-
-#if ("Edm" in row[2]) :
-#    row[2] = row[2].replace("Edm","EDM") 
-#    csv_writer.writerow(row)
-#elif ("Dfw" in row[2] ):
-#    row[2] = row[2].replace("Dfw","DFW") 
-#    csv_writer.writerow(row)
-#elif ("Uk" in row[2]):
-#    row[2] = row[2].replace("Uk","UK")
-#    csv_writer.writerow(row) 
-#elif ("R&b" in row[2]):
-#    row[2] = row[2].replace("R&b","R&B") 
-#    csv_writer.writerow(row)
-#elif ("Lgbtq+" in row[2]):
-#    print(row[2])
-#    row[2] = row[2].replace("Lgbtq+","LGBTQ+") 
-#    print(row[2])
-#    csv_writer.writerow(row)
-#else:
-#    csv_writer.writerow(row)
