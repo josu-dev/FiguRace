c76ef41d76a3bdef0e5b728909716899aaa021da
Josue,Suarez <joosu16@gmail.com>
2022-05-31 20:35:40 -0300

    Removed unnecesary code

--- src/handlers/observer.py
+++ src/handlers/observer.py
@@ -1,34 +1,36 @@
 from typing import Any, Callable
 
 
-subscribers: dict[str, list[Callable[..., Any]]] = dict()
+subscribers: dict[str, list[Callable[..., None]]] = dict()
 
 
-def subscribe(event_type: str, function: Callable[..., Any]) -> None:
+def subscribe(event_type: str, function: Callable[..., None]) -> None:
     if event_type not in subscribers:
         subscribers[event_type] = []
+
     subscribers[event_type].append(function)
 
 
-def unsubscribe(event_type: str, function: Callable[..., Any]) -> None:
+def unsubscribe(event_type: str, function: Callable[..., None]) -> None:
     if event_type not in subscribers:
         return
-    try:
-        subscribers[event_type].remove(function)
-    except ValueError:
-        pass
+
+    subscribers[event_type].remove(function)
 
 
 def post_event(event_type: str, data: Any = None) -> None:
     if event_type not in subscribers:
         return
+
     for function in subscribers[event_type]:
         if data:
             function(data)
         else:
             function()
 
+
 def remove_event(event_type: str) -> None:
     if event_type not in subscribers:
         return
+
     subscribers.pop(event_type)
