03fce4bb90c9e1ffd982b28284a611b18716c274
Josue,Suarez <joosu16@gmail.com>
2022-05-31 16:37:54 -0300

    Refactor app loop and window handling, added timeout event capabilyties

--- src/handlers/window.py
+++ src/handlers/window.py
@@ -1,36 +1,60 @@
+from typing import Any
 import PySimpleGUI as sg
 
 from src import constants
-from src.controllers import theme
-from src.assets import app_icon
 
 from . import observer
 from .screen import Screen, ScreenController
 
 
-screen_controller = ScreenController()
+class WindowController:
+    def __init__(self) -> None:
+        self._screen_ctr = ScreenController()
+        self._window: sg.Window
+        self._timeout: int | None = None
+        self._timeout_key: str = constants.TIME_OUT
+        observer.subscribe(constants.UPDATE_TIMEOUT, self.set_timeout)
 
+    def init(self, screens: list[Screen], initial_screen: str, title: str, app_icon: Any, background_color: str, fullscreen: bool = True) -> None:
+        for screen in screens:
+            self._screen_ctr.register(screen)
 
-def set_up(screens: list[Screen], title: str, initial_screen: str, fullscreen: bool = True) -> sg.Window:
-    for screen in screens:
-        screen_controller.register(screen)
+        self._window = sg.Window(
+            title,
+            self._screen_ctr.composed_layout,
+            icon=app_icon.source,
+            finalize=True,
+            element_justification='center',
+            resizable=True,
+            background_color=background_color,
+            margins=(0, 0)
+        )
 
-    observer.subscribe(constants.GOTO_VIEW, screen_controller.goto_layout)
-    window_layout = screen_controller.composed_layout
+        if fullscreen:
+            self._window.maximize()
 
-    window = sg.Window(
-        title,
-        window_layout,
-        icon=app_icon.source,
-        finalize=True,
-        element_justification='c',
-        resizable=True,
-        background_color=theme.BG_BASE,
-        margins=(0, 0)
-    )
+        self._screen_ctr.init(initial_screen)
 
-    if fullscreen:
-        window.maximize()
+    def loop(self) -> None:
+        try:
+            while True:
+                event, _ = self._window.read(
+                    timeout=self._timeout, timeout_key=self._timeout_key
+                )
+                if event is None or event.startswith(constants.EXIT_APLICATION):
+                    break
 
-    screen_controller.init(initial_screen)
-    return window
+                event = event.split()
+                if len(event) == 1:
+                    observer.post_event(event[0])
+                else:
+                    observer.post_event(event[0], *event[1:])
+        except Exception as error:
+            raise error
+        finally:
+            observer.post_event(constants.EXIT_APLICATION)
+            self._window.close()
+
+    def set_timeout(self, duration: int | None = None, key: str = constants.TIME_OUT) -> None:
+        self._timeout = duration * 1000 if duration else None
+        self._timeout_key = key
--- src/screens/introduction.py
+++ src/screens/introduction.py
@@ -2,53 +2,45 @@ import PySimpleGUI as sg
 
 from src import constants, csg
 from src.controllers import theme
+from src.handlers import observer
 from src.handlers.screen import Screen
-from src.assets import icon, title_name
+from src.assets import title
 
 
 SCREEN_NAME = '-INTRODUCTION-'
-SECONDS = 5
+SECONDS = 3
+BACKGROUND_COLOR = '#000000'
 
 
-_icon = sg.Image(
-        key='-ICON-',
-        data=title_name.source,
-        background_color='black',
-        size=(theme.scale(1920),theme.scale(128)),
-        subsample=title_name.size//theme.scale(1080))
+def create_icon() -> sg.Image:
+    return sg.Image(
+        data=title.source,
+        background_color=BACKGROUND_COLOR,
+        size=(theme.width, theme.scale(256)),
+        subsample=title.size//theme.width
+    )
 
-_black = sg.Button(
-                  key=f'{constants.GOTO_VIEW} -SELECT-PROFILE-',
-                  visible=True,
-                  button_color='Black',
-                  enable_events=True,
-                  expand_y=True,
-                  expand_x=True,
-                  size=(10,10),
-                  border_width=0,
-                  image_data=icon.source,
-                  image_size=(theme.scale(300),theme.scale(300)),
-                  image_subsample=(icon.size // theme.scale(300)),
-                  mouseover_colors='black'
-                  )
+
+def disable_screen() -> None:
+    observer.unsubscribe(constants.TIME_OUT, disable_screen)
+    observer.post_event(constants.UPDATE_TIMEOUT, None)
+    observer.post_event(constants.GOTO_VIEW, '-SELECT-PROFILE-')
+
 
 screen_layout = [
-    [
-        csg.CenteredElement(_icon, background_color='black',)
-    ],
-    [
-        _black
-    ]
+    [csg.CenteredElement(create_icon(), background_color=BACKGROUND_COLOR)],
 ]
-
-def reset() -> None:
-    pass
-
 
 screen_config = {
     'background_color': 'Black',
     'element_justification': 'center',
 }
+
+
+def reset() -> None:
+    observer.subscribe(constants.TIME_OUT, disable_screen)
+    observer.post_event(constants.UPDATE_TIMEOUT, SECONDS)
+
 
 screen = Screen(
     SCREEN_NAME,
