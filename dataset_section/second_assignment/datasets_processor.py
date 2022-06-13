287e1a09281224f1c1727f70c3e13998f2401287
Fabian,Martinez Rincon <fabianmartinezrincon.123@gmail.com>
2022-06-13 19:20:55 -0300

    ordenes de los datasets

--- dataset_section/second_assignment/datasets_processor.py
+++ dataset_section/second_assignment/datasets_processor.py
@@ -1,19 +1,32 @@
 import csv
 import os
 import pandas as pd
-import pandas as to_csv
-
-SAVES_SPOTIFY = ["Top Genre", "Year Released","BPM", "Top Year", "Artist Type","Artist"]
 
 
-path = os.path.join(os.getcwd(),"base_datasets", "Spotify_2010-2019_Top_100.csv")
-data_set = pd.read_csv(path, encoding='utf-8')
-names_compare = (list(map(lambda x: x.lower(), SAVES_SPOTIFY))) 
-df = pd. read_csv (path, usecols = names_compare)
+SAVES_SPOTIFY_LAGOS_FIFA = [["Top Genre", "Year Released","BPM", "Top Year", "Artist Type","Artist"],["Ubicación", "Superficie (km²)", "Profundidad máxima (m)", "Profundidad media (m)", "Coordenadas"],["Age", "Nationality", "Position", "Team" , "Potential","Name"]]
 
-df.to_csv('datos.csv', mode='a')
 
-print(df)
+for save in SAVES_SPOTIFY_LAGOS_FIFA:
+    saves_lower = (list(map(lambda x: x.lower(), save))) 
+
+
+path_base = os.path.join(os.getcwd(),"base_datasets", "Spotify_2010-2019_Top_100.csv")
+
+
+data_set = pd.read_csv(path_base, encoding='utf-8')
+
+df = pd. read_csv (path_base, usecols = saves_spotify)
+
+path_base2 = os.path.join(os.getcwd(),"processed_datasets", 'datos.csv')
+
+df.to_csv(path_base2, mode='a')
+
+
+
+
+
+# Mmmm.... Algo anda mal..
+# Son distintos
 
 
 
