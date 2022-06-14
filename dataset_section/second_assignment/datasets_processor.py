ebddccbf5c4b3710b12bf1381a19fb76e205d51a
Fabian,Martinez Rincon <fabianmartinezrincon.123@gmail.com>
2022-06-13 23:46:31 -0300

    eliminación de columnas y ordenadas

--- dataset_section/second_assignment/datasets_processor.py
+++ dataset_section/second_assignment/datasets_processor.py
@@ -2,37 +2,54 @@ import csv
 import os
 import pandas as pd
 
+SAVES_COLS = {                  
+'FIFA-21_Complete.csv':         ["age", "nationality", "position", "team" , "potential","name"], 
+'Lagos_Argentina - Hoja_1.csv': ["Ubicación", "Superficie (km²)", "Profundidad máxima (m)", "Profundidad media (m)", "Coordenadas"],
+'Spotify_2010-2019_Top_100.csv':["top genre", "year released","bpm", "top year", "artist type","artist"]
+}
 
-SAVES_SPOTIFY_LAGOS_FIFA = [["Top Genre", "Year Released","BPM", "Top Year", "Artist Type","Artist"],["Ubicación", "Superficie (km²)", "Profundidad máxima (m)", "Profundidad media (m)", "Coordenadas"],["Age", "Nationality", "Position", "Team" , "Potential","Name"]]
-
-
-for save in SAVES_SPOTIFY_LAGOS_FIFA:
-    saves_lower = (list(map(lambda x: x.lower(), save))) 
-
-
-path_base = os.path.join(os.getcwd(),"base_datasets", "Spotify_2010-2019_Top_100.csv")
-
-
-data_set = pd.read_csv(path_base, encoding='utf-8')
-
-df = pd. read_csv (path_base, usecols = saves_spotify)
-
-path_base2 = os.path.join(os.getcwd(),"processed_datasets", 'datos.csv')
-
-df.to_csv(path_base2, mode='a')
+ORDER_COLS = {
+'FIFA-21_Complete.csv':         ["team", "nationality", "position", "age", "potential" ,"name"], 
+'Lagos_Argentina - Hoja_1.csv': ["Ubicación", "Superficie (km²)", "Profundidad máxima (m)", "Profundidad media (m)", "Coordenadas"],
+'Spotify_2010-2019_Top_100.csv':["top genre", "artist type", "year released", "top year", "bpm" ,"artist"]
+}
 
 
+PATH_BASE = os.path.join(os.getcwd(),"base_datasets")
+PATH_PROSSED = os.path.join(os.getcwd(),"processed_datasets")
+
+def processed(name_file:str):
+    path_base = os.path.join(PATH_BASE,name_file)
+    print(path_base)
+    path_prossed = os.path.join(PATH_PROSSED,name_file)
+    if name_file in SAVES_COLS:
+        df = pd. read_csv (path_base, sep = r';|,', engine='python',usecols = (SAVES_COLS[name_file]),)
+        df = df[ORDER_COLS[name_file]]
+        
+    df.to_csv(path_prossed, mode='a',index=False)
+
+names_files = os.listdir(PATH_BASE)
+list(map(processed, names_files))
 
 
 
-# Mmmm.... Algo anda mal..
-# Son distintos
 
 
 
-# En formato csv
 
 
-#sep = ';'
 
-#print(data_set)
+
+
+
+
+
+
+
+
+
+
+
+#SAVES_COLS_LOWER = {
+#    key: [item.lower() for item in values] for key,values in SAVES_COLS.items()
+#}
