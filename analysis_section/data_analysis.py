7e0f8b787dac7f4f8359d46a9e5880a9c2b27523
LUCAS ANDRES,GALLARDO FLORIDO <gallardolucas003@gmail.com>
2022-06-16 17:08:14 -0300

    Data analysis

--- analysis_section/data_analysis.py
+++ analysis_section/data_analysis.py
@@ -1,3 +1,7 @@
-import os
+import pandas as pd     
+import os 
 
-print(os.path.dirname(__file__))
+EVENTS_PATH =  os.path.join(os.path.abspath('..'),'src','database','events','events.csv') 
+
+data_frame = pd.read_csv(EVENTS_PATH,encoding='utf-8',index_col='timestamp')
+print(data_frame)
