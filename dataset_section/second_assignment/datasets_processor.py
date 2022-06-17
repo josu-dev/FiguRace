c78e5daadf6350687798082916daba4f76007c87
Fabian,Martinez Rincon <fabianmartinezrincon.123@gmail.com>
2022-06-17 14:58:23 -0300

    corrigiendo el .py de los datasets

--- dataset_section/second_assignment/datasets_processor.py
+++ dataset_section/second_assignment/datasets_processor.py
@@ -28,7 +28,7 @@ POSITION_TABLE_FIFA = {
 
 UPPER_GENDERS_SPOTIFY = ["EDM", "DFW", "UK", "R&B", "LGBTQ+"]
 
-def potential_replace(potential:str):
+def potential_replace(potential):
     compare_potential = int(potential)
     for potential_player in POTENTIAL_TABLE_FIFA:
         if compare_potential >= potential_player:
@@ -36,12 +36,12 @@ def potential_replace(potential:str):
             break
     return potential
 
-def position_replace(position:str):
+def position_replace(position):
     positions = position.split('|')
     position = '|'.join([POSITION_TABLE_FIFA[acronym] for acronym in positions])
     return position
 
-def upper_words(sentence:str):
+def upper_words(sentence):
     """Procesa una frase dependiendo de la consigna"""
     genders = sentence.split()
     for index,gender in enumerate(genders):
@@ -49,14 +49,14 @@ def upper_words(sentence:str):
     sentence = " ".join(genders)
     return sentence
 
-def rebase_coord(coord: str, n_decimals: int = 5) -> str:
+def rebase_coord(coord, n_decimals= 5):
     sign = -1 if 'S' in coord or 'O' in coord else 1
     degree, coord = coord[:-2].split('°')
     min, sec = coord.split('\'')
     dd = sign * (int(degree) + int(min)/60 + int(sec)/3600)
     return str(round(dd, n_decimals)) + '°'
 
-def transform_coords(coords:str) -> str:
+def transform_coords(coords):
     latitude, longitude = coords.split()
     coords = rebase_coord(latitude) + ' ' + rebase_coord(longitude)
     return coords
@@ -95,7 +95,7 @@ PATH_BASE = os.path.dirname(os.path.dirname(__file__))
 PATH_SOURCE = os.path.join(PATH_BASE,"base_datasets")
 PATH_PROSSED = os.path.join(PATH_BASE,"processed_datasets")
 
-def process_dataset(file_name:str):
+def process_dataset(file_name):
     if file_name not in DATASETS:
         return
     file_path = os.path.join(PATH_SOURCE,file_name)
