96a861098505fb5aa1e2e3e2172eb09b7614c898
Fabian,Martinez Rincon <fabianmartinezrincon.123@gmail.com>
2022-06-23 16:57:50 -0300

    comentarios

--- src/handlers/card.py
+++ src/handlers/card.py
@@ -4,12 +4,15 @@ from random import randint
 from . import file
 
 
-class Dataset:
+class Dataset:  # Nombre y todos los datos del dataset
     def __init__(self, name: str, raw_dataset: list[list[str]]) -> None:
         self._name = name
         self._header = raw_dataset[0]
         self._content = raw_dataset[1:]
+        # Genera un objeto iterable (para cualquier tipo de dato) si es un string lo genera random y en si son nros los genera ordenados
         self._used: set[int] = set()
+
+    # Es un decorador que viene por defecto con Python, y puede ser usado para modificar un método para que sea un atributo o propiedad.
 
     @property
     def name(self) -> str:
@@ -19,10 +22,13 @@ class Dataset:
     def header(self) -> list[str]:
         return self._header
 
+    # Genera una fila random
     def _random(self) -> list[str]:
         sample = randint(0, len(self._content) - 1)
         return self._content[sample]
 
+    # Retorna una fila random asegurando de que no se uso
+    # Paso a used el indice de la fila utilizada y me salgo utilizando el breack
     def _random_unique(self) -> list[str]:
         while True:
             sample = randint(0, len(self._content) - 1)
@@ -31,8 +37,10 @@ class Dataset:
                 break
         return self._content[sample]
 
+    # unique: cantidad de filas unicas
     def random_sample(self, size: int, unique: int = 0) -> list[list[str]]:
         samples: list[list[str]] = []
+        # _ en python es descartado (convecion), a nosotros solo nos importa iterar el tamaño
         for _ in range(size):
             if unique > 0:
                 samples.append(self._random_unique())
@@ -47,37 +55,42 @@ class Dataset:
     def reset(self) -> None:
         self._used = set()
 
+#Este módulo proporciona un decorador y funciones para agregar automáticamente métodos especiales generados como __init__() a clases definidas por el usuario.
+# El decorador devuelve la misma clase, no crea ninguna nueva
 
+# Fuente: https://peps.python.org/pep-0557/
+# Solo representa datos
 @dataclass
-class Card:
+class Card: # pista 
     hints: list[str]
     correct_answer: str
     bad_anwers: list[str]
 
 
+
 class CardController:
     def __init__(self, datasets_folder_path: str) -> None:
-        self._folder_path = datasets_folder_path
-        self._find_datasets()
+        self._folder_path = datasets_folder_path #
+        self._find_datasets()  # encontrar dataset (con la lista y los nombres)
         if self.no_datasets:
-            self._load_generic_dataset()
+            self._load_generic_dataset() # Invoco el metodo porque no hay datasets y me quedo con el general
         else:
-            name = list(self._datasets.keys())[0]
+            name = list(self._datasets.keys())[0]# si hay datasets, me quedo con el primero
             self._load_dataset(name)
 
-    def _find_datasets(self) -> None:
-        file.ensure_dirs(self._folder_path)
-        self._datasets = {
-            file_name.split('.')[0]: path
+    def _find_datasets(self) -> None: # find encontrar
+        file.ensure_dirs(self._folder_path) # se asegura de que la carpeta exista
+        self._datasets = {                  # el _folder_path esta como atributo
+            file_name.split('.')[0]: path   # Separamos el archivo de la extension  (generando una lista con sus respectivos nombres)
             for file_name, path in file.scan_dir(self._folder_path, file_extension='.csv')
         }
 
     @property
-    def no_datasets(self) -> bool:
+    def no_datasets(self) -> bool: # Retorna verdadero si no tengo datos en mi dataset
         return len(self._datasets) == 0
 
-    def _load_generic_dataset(self) -> None:
-        self._dataset = Dataset(
+    def _load_generic_dataset(self) -> None: # el metodo genera un dataset con solo nros y de nombre 'Error at loading dataset'
+        self._dataset = Dataset(             
             'Error at loading dataset',
             [
                 [f'empty-{n_rows}-{n_columns}' for n_columns in range(6)]
@@ -85,7 +98,7 @@ class CardController:
             ]
         )
 
-    def _load_dataset(self, name: str) -> None:
+    def _load_dataset(self, name: str) -> None: # Cargo el datases con el nombre y los datos
         raw_dataset = file.load_csv(self._datasets[name], [], read_only=True)
         if raw_dataset == []:
             self._load_generic_dataset()
@@ -112,9 +125,9 @@ class CardController:
     def characteristics(self) -> list[str]:
         return self._dataset.header
 
-    @property
-    def new_card(self) -> Card:
+    @property # hint = pista
+    def new_card(self) -> Card: # Retorna la pregunta correcta y todas las incorrectas
         correct, *bads = self._dataset.random_sample(5, unique=1)
         return Card(
-            correct[:-1], correct[-1], [bad[-1] for bad in bads]
+            correct[:-1], correct[-1], [bad[-1] for bad in bads] #pregunta
         )
--- src/handlers/difficulty.py
+++ src/handlers/difficulty.py
@@ -1,15 +1,23 @@
 from copy import copy
+# Las declaraciones de asignación en Python no copian objetos, crean enlaces entre un objetivo y un objeto. A veces se necesita una copia para poder cambiar una copia sin cambiar la otra.
 from dataclasses import dataclass
 from typing import Any
+# Tipo especial que indica un tipo sin restricciones.
+# Todos los tipos son compatibles con Any.
+# Any es compatible con todos los tipos
 
-from .. import constants, default
-from . import observer, file
+from .. import constants, default # tiene las rutas de los archivos  y default tiene las configuraciones generales del juego, dificultades y colores del juego
+from . import observer, file # El observer 
 
 
 DEFAULT_TYPE = 'normal'
 UPDATE_DIFFICULTY_TYPE = '-UPDATE-DIFFICULTY-TYPE-'
 
+#Este módulo proporciona un decorador y funciones para agregar automáticamente métodos especiales generados como __init__() a clases definidas por el usuario.
+# El decorador devuelve la misma clase, no crea ninguna nueva
 
+# Fuente: https://peps.python.org/pep-0557/
+# Solo representa datos
 @dataclass
 class Difficulty:
     time_per_round: int
@@ -18,13 +26,15 @@ class Difficulty:
     points_bad_answer: int
     characteristics_shown: int
 
-    def swap(self, new: 'Difficulty') -> None:
-        self.time_per_round = new.time_per_round
+    # Cambio los datos dependiendo de la dificultad elegida por el usuario
+    def swap(self, new: 'Difficulty') -> None: # swap/intercambio
+        self.time_per_round = new.time_per_round # new es solo el nombre
         self.rounds_per_game = new.rounds_per_game
         self.points_correct_answer = new.points_correct_answer
         self.points_bad_answer = new.points_bad_answer
         self.characteristics_shown = new.characteristics_shown
 
+    # Retorna un diccionario con todos los datos de la partida
     def jsonify(self) -> dict[str, int]:
         return {
             'time_per_round': self.time_per_round,
@@ -34,18 +44,22 @@ class Difficulty:
             'characteristics_shown': self.characteristics_shown
         }
 
-
+# Cargo las dificultades de json
 class DifficultyController:
     def __init__(self, difficulties_path: str, difficulty: str = DEFAULT_TYPE) -> None:
         self._file_path = difficulties_path
-        self._current_difficulty = difficulty
-        raw_difficulties: dict[str, dict[str, int]] = file.load_json(
+        self._current_difficulty = difficulty # actual
+        raw_difficulties: dict[str, dict[str, int]] = file.load_json( # Cargamos el json con las configuraciones de dificultad
             difficulties_path, default.DIFFICULTIES
         )
-        self._difficulties = {
+        self._difficulties = {# ** la composicion del diccionario (clave,valor(desenpaquetamos el diccionario))
             name: Difficulty(**definition) for name, definition in raw_difficulties.items()
         }
-        self._difficulty = copy(self._difficulties[self._current_difficulty])
+        self._difficulty = copy(self._difficulties[self._current_difficulty]) # Me quedo con una copia de la dificultad actual
+        # Observer es un patrón de diseño de comportamiento que permite a un objeto notificar a otros objetos sobre cambios en su estado
+        # Proporciona una forma de reaccionar a los eventos que suceden en otros objetos, sin acoplarse a sus clases.
+
+        # El controlador se esta suscribiendo al evento "USER_CHANGE" y cuando cambia se ejecuta el metodo "_new_user"
         observer.subscribe(constants.USER_CHANGE, self._new_user)
 
     @property
@@ -56,13 +70,16 @@ class DifficultyController:
     def difficulty_name(self) -> str:
         return self._current_difficulty
 
-    def update_difficulty(
+    def update_difficulty( # update = actualizacion para identificar cuando cambiamos los valores
         self, time_per_round: int | None = None,
         rounds_per_game: int | None = None, points_correct_answer: int | None = None,
         points_bad_answer: int | None = None, characteristics_shown: int | None = None,
     ) -> None:
-        if time_per_round:
+        if time_per_round: # not is none
             self._difficulties['custom'].time_per_round = time_per_round
+        # La equivalente
+        # if time_per_round not is none: 
+        #    self._difficulties['custom'].time_per_round = time_per_round
         if rounds_per_game:
             self._difficulties['custom'].rounds_per_game = rounds_per_game
         if points_correct_answer:
@@ -76,13 +93,18 @@ class DifficultyController:
         self.set_difficulty('custom')
 
     def set_difficulty(self, type: str) -> None:
-        self._current_difficulty = type
+        self._current_difficulty = type 
         self._difficulty.swap(self._difficulties[type])
-        observer.post_event(UPDATE_DIFFICULTY_TYPE, type)
-
-    def difficulties(self) -> dict[str, Difficulty]:
+        # Publico un envento y cambio la dificultad
+        # notifica a posibles suscriptores y la data asociada
+        observer.post_event(UPDATE_DIFFICULTY_TYPE, type) # type informacion asociada al evento
+        
+    # Retorno un diccionario con el nombre de la dificultad
+    # y un objeto que contiene los datos asociados a la dificultad
+    def difficulties(self) -> dict[str, Difficulty]: 
         return {name: copy(definition) for name, definition in self._difficulties.items()}
 
+    # Se modifica solo la dificultad custom ya que las otras dificultades no se tienen que tocar
     def _new_user(self, user: Any) -> None:
         self._difficulties['custom'] = user.custom_difficulty
         self.set_difficulty(user.preferred_difficulty)
